6da15856dfd2c4c8e42d9dd2669253037187f03b
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-05-09 20:41:32 -0400

    set config

---
+++ mf-header/src/root.component.tsx
@@ -0,0 +1,5 @@
+import { App } from "./app";
+
+export default function Root() {
+  return <App/>;
+}
---
+++ mf-users/users/src/app/pages/users/users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-users',
+    templateUrl: './users.component.html',
+    styleUrl: './users.component.css',
+})
+export class UsersComponent {
+
+
+}
